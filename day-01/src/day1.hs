019fa3c66455195fc386aecadf5f557b1fb61d93
acsherrock <acsherrock@gmail.com>
2023-12-01 21:11:39 -0700

    more cleanup

--- day-01/src/day1.hs
+++ day-01/src/day1.hs
@@ -10,22 +10,19 @@ getFL line = [head line] ++ [last line]
 getDigits :: String -> String
 getDigits line = filter isDigit line
 
-strToInt :: String -> Maybe Int
-strToInt str = readMaybe str
-
-fromMaybeNum :: Num a => Maybe a -> a
-fromMaybeNum Nothing = 0
-fromMaybeNum (Just n) = n
+maybeNum :: Num a => Maybe a -> a
+maybeNum Nothing = 0
+maybeNum (Just n) = n
 
 sumList :: Num a => [a] -> a
 sumList [] = 0
 sumList (x:xs) = x + sumList xs
 
 sumCalibrationValues :: String -> String
-sumCalibrationValues input = result
-    where
-        extractValue = fromMaybeNum . strToInt . getFL . getDigits
-        result = show $ sumList (map extractValue (lines input))
+sumCalibrationValues input = show 
+    $ sumList 
+    $ map (maybeNum . readMaybe . getFL . getDigits) 
+    $ lines input
 
 main :: IO()
 main = interact sumCalibrationValues
