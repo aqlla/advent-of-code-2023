8dfbb217f5832e00189f338a6ab3bd56cc0e4645
acsherrock <acsherrock@gmail.com>
2023-12-03 16:39:30 -0700

    cleanup

--- day-01/src/day1.hs
+++ day-01/src/day1.hs
@@ -18,33 +18,19 @@ numTokens = [
 strToInt :: String -> Int
 strToInt = read
   
-applyList :: (a -> b) -> [a] -> [b]
-applyList fn xs = [fn x | x <- xs]
-
 tupCat :: (String, String) -> String
 tupCat tup = L.intercalate "" [fst tup, snd tup]
 
-fromJustTup :: (Maybe a, Maybe b) -> (a, b)
-fromJustTup tup = (fromJust $ fst tup, fromJust $ snd tup)
-  
-justInTup :: (Maybe a, Maybe b) -> Bool
-justInTup (x, y) = True --isJust x || isJust y
+maxMaybe :: Ord a => Maybe a -> Maybe a -> Maybe a
+maxMaybe Nothing y = y
+maxMaybe x Nothing = x
+maxMaybe (Just x) (Just y) = Just $ max x y
 
-minBy :: Ord b => (a -> b) -> [a] -> a
-minBy fn list =
-  case list of
-    (x:y:xs) -> minBy fn (if fn x > fn y then y:xs else x:xs)
-    [x] -> x
-    [] -> error "Empty list"
-    
+minMaybe :: Ord a => Maybe a -> Maybe a -> Maybe a
+minMaybe Nothing y = y
+minMaybe x Nothing = x
+minMaybe (Just x) (Just y) = Just $ min x y
 
-maxBy :: Ord b => (a -> b) -> [a] -> a
-maxBy fn list =
-  case list of
-    (x:y:xs) -> maxBy fn (if fn x < fn y then y:xs else x:xs)
-    [x] -> x
-    [] -> error "Empty list"
-    
 maxByMaybe :: Ord b => (a -> Maybe b) -> a -> a -> a
 maxByMaybe fn x y
   | (isNothing fx) && (isNothing fy) = y
@@ -61,16 +47,6 @@ minByMaybe fn x y
     fx = fn x
     fy = fn y
 
-maxMaybe :: Ord a => Maybe a -> Maybe a -> Maybe a
-maxMaybe Nothing y = y
-maxMaybe x Nothing = x
-maxMaybe (Just x) (Just y) = Just $ max x y
-
-minMaybe :: Ord a => Maybe a -> Maybe a -> Maybe a
-minMaybe Nothing y = y
-minMaybe x Nothing = x
-minMaybe (Just x) (Just y) = Just $ min x y
-
 firstDigit :: [(String, (Maybe Int, Maybe Int))] -> String
 firstDigit xs =
   fst (foldl (minByMaybe (\(_,(lo,_)) -> lo)) (head xs) xs)
@@ -83,12 +59,10 @@ getFirstAndLastDigits :: [(String, (Maybe Int, Maybe Int))] -> (String, String)
 getFirstAndLastDigits ts = (firstDigit ts, lastDigit ts)
   
 anyFirstLastOccursIndex :: String -> [String] -> (String, (Maybe Int, Maybe Int))
-anyFirstLastOccursIndex haystack needles =
-  (
+anyFirstLastOccursIndex haystack needles = (
     last needles, (
       anyFirstOccursIndex haystack needles, 
-      anyLastOccursIndex haystack needles
-  ))
+      anyLastOccursIndex haystack needles))
 
 anyFirstOccursIndex :: String -> [String] -> Maybe Int
 anyFirstOccursIndex haystack needles = first
@@ -105,7 +79,6 @@ anyLastOccursIndex haystack needles = lasst
 firstOccurrenceIndex :: String -> String -> Maybe Int
 firstOccurrenceIndex [] _ = Nothing
 firstOccurrenceIndex haystack needle =
-  -- if L.isPrefixOf needle (traceShowId haystack)
   if L.isPrefixOf needle haystack
     then Just 0
   else case firstOccurrenceIndex (drop 1 haystack) needle of
@@ -115,13 +88,11 @@ firstOccurrenceIndex haystack needle =
 lastOccurrenceIndex :: String -> String -> Maybe Int
 lastOccurrenceIndex [] _ = Nothing
 lastOccurrenceIndex haystack needle =
-  -- if L.isPrefixOf needle (traceShowId haystack)
   if L.isSuffixOf needle haystack
     then Just ((length haystack) - (length needle))
   else case lastOccurrenceIndex (init haystack) needle of
     Just x -> Just(x)
     Nothing -> Nothing
-    
     
 allOccurrenceIndex :: String -> String -> [Int]
 allOccurrenceIndex [] _ = []
@@ -130,7 +101,6 @@ allOccurrenceIndex haystack needle =
     Nothing -> []
     Just 0 -> [0] ++ (allOccurrenceIndex (drop 1 haystack) needle)
     Just x -> [x-1] ++ (allOccurrenceIndex (drop x haystack) needle)
-
 
 doIt :: String -> String
 doIt input =
