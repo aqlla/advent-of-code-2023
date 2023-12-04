359b66ed4e8587f4ef2a2057f651b4642e104f63
Aquilla Sherrock <acsherrock@gmail.com>
2023-12-04 15:08:32 -0700

    day 2 complete

--- day-02/src/day2.hs
+++ day-02/src/day2.hs
@@ -1,6 +1,5 @@
 import qualified Data.List as L
 import qualified Data.Text as T
-import Data.Maybe (fromJust, isJust, isNothing)
 
 strToInt :: String -> Int
 strToInt = read
@@ -8,21 +7,40 @@ strToInt = read
 split :: String -> String -> [String]
 split delim str = map T.unpack $ T.splitOn (T.pack delim) (T.pack str)
 
-extractColor :: [String] -> String -> Int
-extractColor [] _ = 0
-extractColor colors select =
-  case filter (\x -> last x == select) (map (split " ") colors) of
+trd (_,_,x) = x
+
+countColor :: [String] -> String -> Int
+countColor [] _ = 0
+countColor colorStrs select =
+  case filter (\x -> last x == select) (map (split " ") colorStrs) of
     [] -> 0
     [x] -> strToInt $ head x
 
-extractRGB :: String -> [Int]
-extractRGB hand = map (extractColor (split ", " hand)) ["red", "green", "blue"]
+getGameId :: String -> Int
+getGameId lineId = let (_:idStr) = split " " lineId in
+  strToInt $ last $ idStr
 
 parseLine :: String -> (Int, [[Int]])
-parseLine line = do
-  let (idStr:hands) = split ": " line
-  (strToInt $ last $ split " " idStr, 
-   map extractRGB $ split "; " (head hands))
+parseLine line = let (idStr:hand:_) = split ": " line in
+  (getGameId idStr, map countRGB $ split "; " hand)
+
+maxi :: Ord a => [a] -> a
+maxi [x]       = x
+maxi (x:x':xs) = maxi ((if x >= x' then x else x'):xs)
+
+getMaxRGB :: Ord a => [[a]] -> [a]
+getMaxRGB lists = map maxi (L.transpose lists)
+
+countRGB cubesStr = map (countColor (split ", " cubesStr)) ["red", "green", "blue"]
+
+isPossible :: [Int] -> Bool
+isPossible (r:g:b:_) = 12 >= r && 13 >= g && 14 >= b
+
+filterPossible = filter (\x -> isPossible (getMaxRGB (snd x)))
+
+part1 input = sum $ map fst $ filterPossible $ map parseLine $ lines $ input
+part2 input = sum $ map (\x -> foldr (*) 1 (getMaxRGB (snd x))) $ map parseLine $ input
 
 main :: IO()
-main = interact (\input -> show $ map parseLine $ lines $ input)
+main = interact (\input -> show $ part2 $ lines $ input)
+-- main = interact (\input -> show $ sum $ map fst $ filterPossible $ map parseLine $ lines $ input)
