8123f6f54cdb5541a171efee5c904eff41202d6d
Aquilla Sherrock <acsherrock@gmail.com>
2023-12-04 15:12:57 -0700

    day 2 cleanup

--- day-02/src/day2.hs
+++ day-02/src/day2.hs
@@ -7,7 +7,12 @@ strToInt = read
 split :: String -> String -> [String]
 split delim str = map T.unpack $ T.splitOn (T.pack delim) (T.pack str)
 
-trd (_,_,x) = x
+maxi :: Ord a => [a] -> a
+maxi [x] = x
+maxi (x:x':xs) = maxi ((if x >= x' then x else x'):xs)
+
+getMaxRGB :: Ord a => [[a]] -> [a]
+getMaxRGB lists = map maxi (L.transpose lists)
 
 countColor :: [String] -> String -> Int
 countColor [] _ = 0
@@ -21,23 +26,15 @@ getGameId lineId = let (_:idStr) = split " " lineId in
   strToInt $ last $ idStr
 
 parseLine :: String -> (Int, [[Int]])
-parseLine line = let (idStr:hand:_) = split ": " line in
+parseLine line = 
+  let (idStr:hand:_) = split ": " line;
+      countRGB cubesStr = map (countColor (split ", " cubesStr)) ["red", "green", "blue"] in
   (getGameId idStr, map countRGB $ split "; " hand)
-
-maxi :: Ord a => [a] -> a
-maxi [x]       = x
-maxi (x:x':xs) = maxi ((if x >= x' then x else x'):xs)
-
-getMaxRGB :: Ord a => [[a]] -> [a]
-getMaxRGB lists = map maxi (L.transpose lists)
-
-countRGB cubesStr = map (countColor (split ", " cubesStr)) ["red", "green", "blue"]
 
 isPossible :: [Int] -> Bool
 isPossible (r:g:b:_) = 12 >= r && 13 >= g && 14 >= b
 
 filterPossible = filter (\x -> isPossible (getMaxRGB (snd x)))
-
 part1 input = sum $ map fst $ filterPossible $ map parseLine $ lines $ input
 part2 input = sum $ map (\x -> foldr (*) 1 (getMaxRGB (snd x))) $ map parseLine $ input
 
