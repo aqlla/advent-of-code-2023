cf3c237d6a8b050e2f05043dae84ff86c77b979b
acsherrock <acsherrock@gmail.com>
2023-12-04 01:33:51 -0700

    day 2 almost

--- day-02/src/day2.hs
+++ day-02/src/day2.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 import qualified Data.List as L
 import qualified Data.Text as T
 import Data.Maybe (fromJust, isJust, isNothing)
@@ -7,27 +5,24 @@ import Data.Maybe (fromJust, isJust, isNothing)
 strToInt :: String -> Int
 strToInt = read
 
-extractColor :: [T.Text] -> T.Text -> Int
-extractColor [] _ = 0
-extractColor colors select = 
-  case filter (\x -> last x == select) colorsSplit of
-    [] -> 0
-    [x] -> strToInt $ T.unpack $ head x
-  where
-    colorsSplit = map (T.splitOn " ") colors
+split :: String -> String -> [String]
+split delim str = map T.unpack $ T.splitOn (T.pack delim) (T.pack str)
 
-extractRGB :: T.Text -> [Int]
-extractRGB hand = map (extractColor (T.splitOn ", " hand)) ["red", "green", "blue"]
+extractColor :: [String] -> String -> Int
+extractColor [] _ = 0
+extractColor colors select =
+  case filter (\x -> last x == select) (map (split " ") colors) of
+    [] -> 0
+    [x] -> strToInt $ head x
+
+extractRGB :: String -> [Int]
+extractRGB hand = map (extractColor (split ", " hand)) ["red", "green", "blue"]
 
 parseLine :: String -> (Int, [[Int]])
-parseLine line = (
-    strToInt $ T.unpack $ last $ T.splitOn " " idStr,
-    map extractRGB $ T.splitOn "; " (head hands))
-  where 
-    (idStr:hands) = T.splitOn ": " (T.pack line)
-
-doIt :: String -> String
-doIt input = show $ map parseLine $ lines $ input
+parseLine line = do
+  let (idStr:hands) = split ": " line
+  (strToInt $ last $ split " " idStr, 
+   map extractRGB $ split "; " (head hands))
 
 main :: IO()
-main = interact doIt
+main = interact (\input -> show $ map parseLine $ lines $ input)
