854c06f96b0de2295732b1303741a28e425f68dc
Aquilla Sherrock <acsherrock@gmail.com>
2023-12-04 15:48:44 -0700

    day 2 cleanup

--- day-02/src/day2.hs
+++ day-02/src/day2.hs
@@ -12,7 +12,7 @@ maxi [x] = x
 maxi (x:x':xs) = maxi ((if x >= x' then x else x'):xs)
 
 getMaxRGB :: Ord a => [[a]] -> [a]
-getMaxRGB lists = map maxi (L.transpose lists)
+getMaxRGB lists = map maxi . L.transpose $ lists
 
 countColor :: [String] -> String -> Int
 countColor [] _ = 0
@@ -23,20 +23,19 @@ countColor colorStrs select =
 
 getGameId :: String -> Int
 getGameId lineId = let (_:idStr) = split " " lineId in
-  strToInt $ last $ idStr
+  strToInt . last $ idStr
 
 parseLine :: String -> (Int, [[Int]])
 parseLine line = 
-  let (idStr:hand:_) = split ": " line;
-      countRGB cubesStr = map (countColor (split ", " cubesStr)) ["red", "green", "blue"] in
+  let countRGB cubesStr = map (countColor (split ", " cubesStr)) ["red", "green", "blue"]; 
+      (idStr:hand:_) = split ": " line in
   (getGameId idStr, map countRGB $ split "; " hand)
 
 isPossible :: [Int] -> Bool
 isPossible (r:g:b:_) = 12 >= r && 13 >= g && 14 >= b
 
 filterPossible = filter (\x -> isPossible (getMaxRGB (snd x)))
-part1 input = sum $ map fst $ filterPossible $ map parseLine $ lines $ input
-part2 input = sum $ map (\x -> foldr (*) 1 (getMaxRGB (snd x))) $ map parseLine $ input
+part1 input = sum . map fst . filterPossible . map parseLine . lines $ input
+part2 input = sum . map (\x -> foldr (*) 1 (getMaxRGB . snd $ x)) . map parseLine $ input
 
-main :: IO()
-main = interact (\input -> show $ part2 $ lines $ input)
+main = interact (show . part2 . lines)
